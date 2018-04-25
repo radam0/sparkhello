48083df5d6a19083f7db2683e25cecab55d495c4
vnikhil5 <vemulanikhil23@gmail.com>
2018-04-24 20:08:49 -0400

    Initial commit

---
+++ src/main/scala/HelloWorld.scala
@@ -0,0 +1,8 @@
+object HelloWorld {
+  def main(args: Array[String]): Unit = {
+    println("Hello World, welcome to scala demo")
+    //    println("Hello " + args(0)) // used to pass arguments Run-Edit configuration-Application-give the class name-pass arguments
+
+  }
+
+}
